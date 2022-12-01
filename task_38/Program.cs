d23ac83e59bb6d4d28d4dcf1c9440fbc12de76c8
leinore <116540768+leinore@users.noreply.github.com>
2022-12-01 08:14:58 +0300

    add task_38

---
+++ task_38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных(тип double) чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] FillArray(int size)
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(new Random().NextDouble()*100, 1);
+    }
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] array = FillArray(num);
+
+Console.WriteLine(string.Join(",", array));
+
+double max = array[0];
+double min = array[0];
+double difference = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array [i];
+    }
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+        
+    }
+
+difference = max + min;
+Console.WriteLine(min);
+Console.WriteLine(max);
+Console.WriteLine(difference);
